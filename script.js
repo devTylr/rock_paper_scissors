c86fcf2525a9623351aeb90bfac03ed39c0949da
devTylr <tyler.ward1199@gmail.com>
2023-08-22 17:53:49 -0400

    Added simple rock, paper, scissors UI

--- script.js
+++ script.js
@@ -13,36 +13,64 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    const results = document.querySelector('.results');
+    const score = document.querySelector('.score');
+    const para = document.createElement('p');
+    
     if (playerSelection === 'rock' && computerSelection === 'rock' // if TIE game
     || playerSelection === 'paper' && computerSelection === 'paper'
     || playerSelection === 'scissors' && computerSelection === 'scissors') {
-        console.log('Tie round');
+        score.textContent = `Player Score: ${pScore} Computer Score: ${cScore}`;
+        results.textContent = 'Tie round';
     } else if (playerSelection === 'rock' && computerSelection === 'scissors' // if PLAYER wins
     || playerSelection === 'scissors' && computerSelection === 'paper'
     || playerSelection === 'paper' && computerSelection === 'rock') {
-        console.log('Round won!');
         pScore++;
+        score.textContent = `Player Score: ${pScore} Computer Score: ${cScore}`;
+        results.textContent = 'You won that round!';
     } else { // if YOU LOSE
-        console.log('Round lost :(');
         cScore++;
+        score.textContent = `Player Score: ${pScore} Computer Score: ${cScore}`;
+        results.textContent = 'You lost that round...';
+    }
+
+    if (pScore === 5 && cScore < 5) {
+        results.textContent = 'Game over. You win the game!';
+    } else if (cScore === 5 && pScore < 5) {
+        results.textContent = 'Game over. You lost the game!';
     }
 }
 
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('.paper');
+const scissorsButton = document.querySelector('.scissors');
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        const playerSelection = prompt('question').toLowerCase();
-        const computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-        console.log(pScore, cScore);
-    }
+rockButton.addEventListener('click', () => {
+    playRound('rock', getComputerChoice());
+});
+paperButton.addEventListener('click', () => {
+    playRound('paper', getComputerChoice());
+});
+scissorsButton.addEventListener('click', () => {
+    playRound('scissors', getComputerChoice());
+});
 
-    if (pScore > cScore) {
-        console.log(`You won!`);
-    } else if (pScore < cScore) {
-        console.log(`You lost.`);
-    } else {
-        console.log(`Tie game.`)
-    }
-}
-game();
+// function game() {
+
+
+//     for (let i = 0; i < 5; i++) {       // play 5 rounds
+//         const playerSelection = prompt('question').toLowerCase();
+//         const computerSelection = getComputerChoice();
+//         playRound(playerSelection, computerSelection);
+//         console.log(pScore, cScore);
+//     }
+
+//     if (pScore > cScore) {       // win / lose message
+//         console.log(`You won!`);
+//     } else if (pScore < cScore) {
+//         console.log(`You lost.`);
+//     } else {
+//         console.log(`Tie game.`)
+//     }
+// }
+// game();
