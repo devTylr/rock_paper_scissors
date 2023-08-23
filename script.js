de16258ff13b897db2dc4444114e945d66db21fd
devTylr <tyler.ward1199@gmail.com>
2023-08-23 08:29:57 -0400

    Added color and spacing to ui

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const rockButton = document.querySelector('.rock');
+const paperButton = document.querySelector('.paper');
+const scissorsButton = document.querySelector('.scissors');
 let pScore = 0;
 let cScore = 0;
 
@@ -15,7 +18,6 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     const results = document.querySelector('.results');
     const score = document.querySelector('.score');
-    const para = document.createElement('p');
     
     if (playerSelection === 'rock' && computerSelection === 'rock' // if TIE game
     || playerSelection === 'paper' && computerSelection === 'paper'
@@ -36,43 +38,23 @@ function playRound(playerSelection, computerSelection) {
 
     if (pScore === 5 && cScore < 5) {
         results.textContent = 'Game over. You win the game!';
-        
+        rockButton.removeEventListener('click', rockEventHandler);
+        paperButton.removeEventListener('click', paperEventHandler);
+        scissorsButton.removeEventListener('click', scissorsEventHandler);
     } else if (cScore === 5 && pScore < 5) {
         results.textContent = 'Game over. You lost the game!';
-        
+        rockButton.removeEventListener('click', rockEventHandler);
+        paperButton.removeEventListener('click', paperEventHandler);
+        scissorsButton.removeEventListener('click', scissorsEventHandler);
     }
 }
 
-const rockButton = document.querySelector('.rock');
-const paperButton = document.querySelector('.paper');
-const scissorsButton = document.querySelector('.scissors');
+// Created eventHandlers so that we can removeEventListener inside playRound()
+function rockEventHandler() {playRound('rock', getComputerChoice())};
+function paperEventHandler() {playRound('paper', getComputerChoice())};
+function scissorsEventHandler() {playRound('scissors', getComputerChoice())};
 
-rockButton.addEventListener('click', () => {
-    playRound('rock', getComputerChoice());
-});
-paperButton.addEventListener('click', () => {
-    playRound('paper', getComputerChoice());
-});
-scissorsButton.addEventListener('click', () => {
-    playRound('scissors', getComputerChoice());
-});
-
-// function game() {
-
-
-//     for (let i = 0; i < 5; i++) {       // play 5 rounds
-//         const playerSelection = prompt('question').toLowerCase();
-//         const computerSelection = getComputerChoice();
-//         playRound(playerSelection, computerSelection);
-//         console.log(pScore, cScore);
-//     }
-
-//     if (pScore > cScore) {       // win / lose message
-//         console.log(`You won!`);
-//     } else if (pScore < cScore) {
-//         console.log(`You lost.`);
-//     } else {
-//         console.log(`Tie game.`)
-//     }
-// }
-// game();
+// Button Listeners
+rockButton.addEventListener('click', rockEventHandler);
+paperButton.addEventListener('click', paperEventHandler);
+scissorsButton.addEventListener('click', scissorsEventHandler);
